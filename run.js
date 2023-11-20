af6c3fbf15b48ae2675220f309f2bd64d8e519ae
Sean Goedecke <sgoedecke@github.com>
2023-11-21 08:57:54 +1100

    work

--- run.js
+++ run.js
@@ -60,16 +60,24 @@ offset += weights.w3.byteLength;
 weights.rmsFinalWeight = new Float32Array(checkpoint.buffer, offset, config.dim);
 offset += weights.rmsFinalWeight.byteLength;
 
-offset += config.seqLen * headSize / 2;
-offset += config.seqLen * headSize / 2;
+// this is temporary - seeing if the cpp style RoPE works
+weights.freqCisReal = new Float32Array(checkpoint.buffer, offset, config.seqLen * headSize / 2);
+offset += weights.freqCisReal.byteLength;
+weights.freqCisImag = new Float32Array(checkpoint.buffer, offset, config.seqLen * headSize / 2);
+offset += weights.freqCisImag.byteLength;
+
+// offset += config.seqLen * headSize / 2; // run.cpp reads in freq_cis_real from here but run.c now skips it
+// offset += config.seqLen * headSize / 2;
 
 weights.wcls = weights.tokenEmbeddingTable; // no shared weights
 
 // We've read out all the content from the checkpoint, but we're not in C anymore and we don't have to pretend that our
 // 2D arrays are represented by a 1D array. Let's fix that.
 
-function splitFloat32Array(array, chunkSize) {
+function splitFloat32Array(array, numChunks) {
     const result = [];
+
+    const chunkSize = array.length / numChunks
     for (let i = 0; i < array.length; i += chunkSize) {
         const end = Math.min(i + chunkSize, array.length);
         result.push(array.subarray(i, end));
@@ -77,17 +85,33 @@ function splitFloat32Array(array, chunkSize) {
     return result;
 }
 
-weights.tokenEmbeddingTable = splitFloat32Array(weights.tokenEmbeddingTable, config.dim);
-weights.rmsAttWeight = splitFloat32Array(weights.rmsAttWeight, config.dim);
-weights.wq = splitFloat32Array(weights.wq, config.dim ** 2).map((array) => splitFloat32Array(array, config.dim));
-weights.wk = splitFloat32Array(weights.wk, config.dim ** 2).map((array) => splitFloat32Array(array, config.dim));
-weights.wv = splitFloat32Array(weights.wv, config.dim ** 2).map((array) => splitFloat32Array(array, config.dim));
-weights.wo = splitFloat32Array(weights.wo, config.dim ** 2).map((array) => splitFloat32Array(array, config.dim));
-weights.rmsFFNWeight = splitFloat32Array(weights.rmsFFNWeight, config.dim);
-weights.w1 = splitFloat32Array(weights.w1, config.dim * config.hiddenDim).map((array) => splitFloat32Array(array, config.dim));
-// w2 has to be backwards? or something? skip for now, we'll just use lengths
-// weights.w2 = splitFloat32Array(weights.w2, config.hiddenDim * config.dim).map((array) => splitFloat32Array(array, config.dim));
-weights.w3 = splitFloat32Array(weights.w3, config.dim * config.hiddenDim).map((array) => splitFloat32Array(array, config.dim));
+// Let's resize the 1d weights arrays into 2d and 3d tensors
+weights.tokenEmbeddingTable = splitFloat32Array(weights.tokenEmbeddingTable, config.vocabSize); // [vocabSize, dim]
+weights.rmsAttWeight = splitFloat32Array(weights.rmsAttWeight, config.nLayers); // [layers, dim]
+weights.rmsFFNWeight = splitFloat32Array(weights.rmsFFNWeight, config.nLayers); // [layers, dim]
+
+// All these are attention matmuls with the 3d tensor format [layers, [dim, dim]]
+weights.wq = splitFloat32Array(weights.wq, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.dim)) // [layers, [dim, dim]]
+weights.wk = splitFloat32Array(weights.wk, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.dim)); // [layers, [dim, dim]]
+weights.wv = splitFloat32Array(weights.wv, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.dim));
+weights.wo = splitFloat32Array(weights.wo, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.dim));
+
+// These are the FFN matmuls with the 3d tensor format [layers, [dim, hiddenDim]]
+weights.w1 = splitFloat32Array(weights.w1, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.hiddenDim));
+weights.w2 = splitFloat32Array(weights.w2, config.nLayers) // w2 is [layers, [hiddenDim, dim]]
+    .map((array) => splitFloat32Array(array, config.dim));
+weights.w3 = splitFloat32Array(weights.w3, config.nLayers)
+    .map((array) => splitFloat32Array(array, config.hiddenDim));
+
+weights.freqCisReal = splitFloat32Array(weights.freqCisReal, config.seqLen);
+weights.freqCisImag = splitFloat32Array(weights.freqCisImag, config.seqLen);
+
+
 weights.wcls = weights.tokenEmbeddingTable
 
 console.log("Constructed weights:", weights)
@@ -120,13 +144,10 @@ for (let i = 0; i < config.vocabSize; i++) {
 // we're skipping byte pieces and sortedVocab as unnecessary for now
 // time to generate! let's start by encoding a prompt
 
-const prompt = "Zap"
+const prompt = "Once upon a"
 const promptTokens = []
 
 prompt.split("").forEach((char) => {
-    if (char == " ") {
-        char = "<0x20>" 
-    }
     promptTokens.push(tokenizer.vocab.indexOf(char))
 })
 
@@ -155,6 +176,7 @@ for (let i = 0; i < tries; i++) {
 
 const steps = 50 // the -n param in llama2.c
 let pos = 0
+promptTokens.unshift(1) // 1 = BOS token in Llama-2 sentence-piece
 const numPromptTokens = promptTokens.length
 let token  = promptTokens[0]
 
@@ -168,35 +190,26 @@ const runState = {
     q: new Float32Array(config.dim), // query (dim,)
     k: new Float32Array(config.dim), // key (dim,)
     v: new Float32Array(config.dim), // value (dim,)
-    att: new Float32Array(config.nHeads * config.seqLen), // buffer for scores/attention values (n_heads, seq_len)
+    att: new Float32Array(config.seqLen), // buffer for scores/attention values // run.js has (n_heads, seq_len) instead
     logits: new Float32Array(config.vocabSize), // output logits
     // kv cache
     keyCache: new Float32Array(config.nLayers * config.seqLen * config.dim),   // (layer, sq_len, dim)
     valueCache: new Float32Array(config.nLayers * config.seqLen * config.dim), // (layer, seq_len, dim)
 };
+runState.keyCache = splitFloat32Array(runState.keyCache, config.nLayers)
+    .map((arr) => splitFloat32Array(runState.keyCache, config.seqLen)) // 3d tensor
+runState.valueCache = splitFloat32Array(runState.valueCache, config.nLayers)
+.map((arr) => splitFloat32Array(runState.valueCache, config.seqLen)) // 3d tensor
 
 const kvDim = (config.dim * config.nKVHeads) / config.nHeads
 const kvMul = config.nHeads / config.nKVHeads
 
-function matmul(xout, x, w, n=null, d=null) {
-    if (typeof w[0] == 'object') { // if we have an arr of arrs
-        // w = new Float32Array(w.reduce((acc, val) => [...acc, ...Array.from(val)], []));
-    }
-
-    n = n || x.length
-    d = d || w.length
-
-    
-    for (let i = 0; i < d; i++) {
-        let val = 0;
-        for (let j = 0; j < n; j++) {
-            val += w[i][j] * x[j];
+function matmul(output, input, weight) {
+    for (let i = 0; i < output.length; i++) {
+        output[i] = 0;
+        for (let j = 0; j < input.length; j++) {
+            output[i] += input[j] * weight[i][j];
         }
-        xout[i] = val; 
-    }
-    if (x.length > n || w.length > (d*n)) {
-        debugger
-        throw "dimensions do not match"
     }
 }
 
@@ -217,10 +230,7 @@ function rmsnorm(o, x, weight) {
     }
 }
 
-function softmax(x, size) {
-    
-    // const size = x.length
-    
+function softmax(x, size) {   
     // find max value 
     let maxVal = x[0];
     for (let i = 1; i < size; i++) {
@@ -244,116 +254,122 @@ function softmax(x, size) {
     
 }
 
+function randomSample(array) {
+    const index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
+
 while (pos < steps) {
     // forward pass
     // copy the token embedding into x
-    runState.x = weights.tokenEmbeddingTable[token]
+    runState.x = new Float32Array(weights.tokenEmbeddingTable[token])
     
     // forward each layer
-    for (let l = 0; l < config.nLayers; l++) {
+    for (let layer = 0; layer < config.nLayers; layer++) {
 
-        if (runState.x == undefined) {
-            debugger
-        }
+        rmsnorm(runState.xb, runState.x, weights.rmsAttWeight[layer]) // attention rmsnorm
 
-        rmsnorm(runState.xb, runState.x, weights.rmsAttWeight[l]) // attention rmsnorm
-
-        // qkv matmuls
-        matmul(runState.q, runState.xb, weights.wq[l], config.dim, config.dim)
-        matmul(runState.k, runState.xb, weights.wk[l], config.dim, kvDim)
-        matmul(runState.v, runState.xb, weights.wv[l], config.dim, kvDim)
+        // attention qkv matmuls
+        matmul(runState.q, runState.xb, weights.wq[layer])
+        matmul(runState.k, runState.xb, weights.wk[layer])
+        matmul(runState.v, runState.xb, weights.wv[layer])
 
         // RoPE relative pos encoding: complex-valued rotate q and k in each head
-        for (let i = 0; i < config.dim; i += 2) {
-            const headDim = i % headSize;
-            const freq = 1 / Math.pow(10000, headDim / headSize);    
-            const val = pos * freq;
-            const fcr = Math.cos(val);
-            const fci = Math.sin(val);
-            const rotn = i < kvDim ? 2 : 1; 
-            for (let v = 0; v < rotn; v++) {
-                let vec = v === 0 ? runState.q : runState.k; // this NaNs them
-                const v0 = vec[i];
-                const v1 = vec[i+1];
+        // llama2.c iterates two-at-a-time up dim, but this is the same as going by head size
+        // for (let i = 0; i < config.dim; i += 2) {
+        //     const headDim = i % headSize;
+        //     const freq = 1 / Math.pow(10000, headDim / headSize);    
+        //     const val = pos * freq;
+        //     const fcr = Math.cos(val);
+        //     const fci = Math.sin(val);
+        //     const rotn = i < kvDim ? 2 : 1; 
+        //     for (let v = 0; v < rotn; v++) {
+        //         let vec = v === 0 ? runState.q : runState.k; // this NaNs them
+        //         const v0 = vec[i];
+        //         const v1 = vec[i+1];
 
-                vec[i] = v0 * fcr - v1 * fci;
-                vec[i+1] = v0 * fci + v1 * fcr;
+        //         vec[i] = v0 * fcr - v1 * fci;
+        //         vec[i+1] = v0 * fci + v1 * fcr;
+        //     }
+        // }
+        for (let head = 0; head < config.nHeads; head++) {
+            const start = head * headSize;
+            for (let i = 0; i < headSize; i += 2) {
+                const q0 = runState.q[start + i];  
+                const q1 = runState.q[start + i + 1];
+                const k0 = runState.k[start + i];
+                const k1 = runState.k[start + i + 1];
+                const fcr = weights.freqCisReal[pos][i / 2]
+                const fci = weights.freqCisImag[pos][i / 2]
+                debugger
+                runState.q[start + i] = q0 * fcr - q1 * fci;
+                runState.q[start + i + 1] = q0 * fci + q1 * fcr;
+                runState.k[start + i] = k0 * fcr - k1 * fci;
+                runState.k[start + i + 1] = k0 * fci + k1 * fcr;
             }
-
         }
+        debugger
 
-        // populate key/value caches. llama.c does this way above here
-        // TODO: skip the cache layer offset and use actual arrays
-        const loff = l * config.seqLen * kvDim;
 
-        const keyCacheRow = runState.keyCache.subarray(loff + pos * kvDim, loff + (pos + 1) * kvDim);
-        const valueCacheRow = runState.valueCache.subarray(loff + pos * kvDim, loff + (pos + 1) * kvDim);
-        keyCacheRow.set(runState.k);
-        valueCacheRow.set(runState.v);
+        // populate key/value caches. llama.c does this way above here but all ports do this here
+        runState.keyCache[layer][pos] = new Float32Array(runState.k)
+        runState.valueCache[layer][pos] = new Float32Array(runState.v)
 
         // multi-head attention, iterate over all heads. could parallelize this in theory.
-        for (let h = 0; h < config.nHeads; h++) {
-            // get q vector for this head  
-            const q = runState.q.subarray(h * headSize, (h + 1) * headSize);
-            // att scores for this head
-            const att = runState.att.subarray(h * config.seqLen, (h + 1) * config.seqLen);
-            
-            for (let t = 0; t <= pos; t++) {
-                // get k vector
-                const k = runState.keyCache.subarray(t * kvDim + Math.floor(h / kvMul) * headSize, t * kvDim + Math.floor(h / kvMul) * headSize + headSize);
-                
+        for (let head = 0; head < config.nHeads; head++) {          
+            for (let t = 0; t <= pos; t++) {              
                 // att score
                 let score = 0;
                 for (let i = 0; i < headSize; i++) {
-                    score += q[i] * k[i];
+                    // hmm
+                    score += runState.q[head * headSize + i] * runState.keyCache[layer][pos][head * headSize + i];
                 }
                 score /= Math.sqrt(headSize);
                 // save to att buffer  
-                att[t] = score; 
+                runState.att[t] = score; 
             }
             // softmax att weights  
-            softmax(att, pos + 1);
+            softmax(runState.att, pos + 1);
 
             // weighted sum of v into xb
-            const xb = runState.xb.subarray(h * headSize, (h + 1) * headSize);
-            xb.fill(0);
-            for (let t = 0; t <= pos; t++) {
-                const v = runState.valueCache.subarray(t * kvDim + Math.floor(h / kvMul) * headSize, t * kvDim + Math.floor(h / kvMul) * headSize + headSize);
-                const a = att[t];
-                for (let i = 0; i < headSize; i++) {
-                    xb[i] += a * v[i];
+            for (let i = 0; i < headSize; i++) {
+                runState.xb[head * headSize + i] = 0;
+                for (let t = 0; t <= pos; t++) {
+                    runState.xb[head * headSize + i] += runState.att[t] * runState.valueCache[layer][t][head * headSize + i];
                 }
             }
+
 
         }
 
         // final matmul to get the output of the attention
-        matmul(runState.xb2, runState.xb, weights.wo[l], config.dim, config.dim)
+        matmul(runState.xb2, runState.xb, weights.wo[layer])
 
         // residual connection 
-        for (let i = 0; i < config.dim; i++) {
+        for (let i = 0; i < runState.xb2.length; i++) { // should be config.dim
             runState.x[i] += runState.xb2[i]; 
         }
 
-        rmsnorm(runState.xb, runState.x, weights.rmsFFNWeight[l]) // ffn rmsnorm
+        rmsnorm(runState.xb, runState.x, weights.rmsFFNWeight[layer]) // ffn rmsnorm
         
-        matmul(runState.hb, runState.xb, weights.w1[l], config.dim, config.hiddenDim)
-        matmul(runState.hb2, runState.xb, weights.w3[l], config.dim, config.hiddenDim)
+        matmul(runState.hb, runState.xb, weights.w1[layer])
+        matmul(runState.hb2, runState.xb, weights.w3[layer])
         
         // SwiGLU non-linearity
         for (let i = 0; i < config.hiddenDim; i++) {
-            let val = runState.hb[i];
-            val *= 1 / (1 + Math.exp(-val)); 
-            val *= runState.hb2[i];
-            runState.hb[i] = val;
+            runState.hb[i] = runState.hb[i] * (1.0 / (1.0 + Math.exp(-runState.hb[i])));
+        }
+        // elementwise mult with w3(x)
+        for (let i = 0; i < config.hiddenDim; i++) {
+            runState.hb[i] = runState.hb[i] * runState.hb[i];
         }
 
-        // final matmul for ffn output
-        // this NaNs xb without explicitly passing the lengths. TODO: figure out how to do this with arrays
-        matmul(runState.xb, runState.hb, weights.w2.subarray(l * config.hiddenDim * config.dim, (l + 1) * config.hiddenDim * config.dim), config.hiddenDim, config.dim); // dimensions don't match
 
+        // final matmul for ffn output
+        matmul(runState.xb, runState.hb, weights.w2[layer])
+        
         // residual connection
-        for (let i = 0; i < config.dim; i++) {
+        for (let i = 0; i < runState.xb.length; i++) {
             runState.x[i] += runState.xb[i];
         }
         
@@ -363,13 +379,17 @@ while (pos < steps) {
     rmsnorm(runState.x, runState.x, weights.rmsFinalWeight);
 
     // classifier into logits
-    matmul(runState.logits, runState.x, weights.wcls, config.dim, config.vocabSize);
+    matmul(runState.logits, runState.x, weights.wcls); // could I just reuse tokenEmbeddingTable? some ports do
 
     if (pos < numPromptTokens) {
         // still in prompt
         token = promptTokens[pos]
     } else {
-        token = runState.logits.indexOf(Math.max(...runState.logits))
+        // simplest possible topp over the logits
+        debugger
+        const numToSample = 5
+        const topp = Array.from(runState.logits).sort((a, b) => b - a).slice(0, numToSample).map(x => runState.logits.indexOf(x))//.map(x => tokenizer.vocab[x])
+        token = randomSample(topp)
     }
 
     let piece = tokenizer.vocab[token]
